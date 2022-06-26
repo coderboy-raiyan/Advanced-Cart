077da277affd4fd7a24fa8c177cb5d5bf3ca932b
Raiyan <tajkierhaque@gmail.com>
2022-06-26 13:44:16 +0600

    Sass added

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import "App.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "redux/store/store";
+import "styles/App.scss";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
