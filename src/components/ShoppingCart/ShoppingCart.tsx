92f0260ceb8f995f30992ae6b6de7157f202d765
Raiyan <tajkierhaque@gmail.com>
2022-06-29 13:57:47 +0600

    cart open close done

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import { AiOutlineClose } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { closeCart } from "redux/actions/cartAction";
@@ -11,9 +13,13 @@ function ShoppingCart() {
     return (
         <>
             {isCartOpen && (
-                <div className="fixed top-0 left-0 right-0 bottom-0 z-[100] bg-[rgba(0,0,0,.3)]" />
+                <div
+                    onClick={() => dispatch(closeCart(false))}
+                    className="fixed top-0 left-0 right-0 bottom-0 z-[100] bg-[rgba(0,0,0,.3)]"
+                />
             )}
             <div
+                onClick={() => dispatch(closeCart(false))}
                 className={`fixed ${
                     isCartOpen ? "right-0" : "-right-[100%]"
                 } top-0 z-[101] h-full  bg-white shadow transition-all lg:w-[25%]`}
